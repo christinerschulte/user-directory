77944d23e3360fff72c737a03471aa0ff7ef5b8d
Christine Schulte <christinerschulte@gmail.com>
2018-05-15 14:52:43 -0400

    Add each user in its own paragraph

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const handleSubmit = function(ev){
     const users = document.querySelector("#users")
     const f = ev.target
     const userName = f.newText.value
-    users.textContent += ' ' + userName
+    users.innerHTML += '<p>' + userName + '</p>'
     f.newText.value = ''
 
     
