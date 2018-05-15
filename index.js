3cbc6394c2db8d86697041ca427b7cc8a45cf046
Christine Schulte <christinerschulte@gmail.com>
2018-05-15 14:19:38 -0400

    Update the heading with a value from the form - revised

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
+const form = document.querySelector('#myForm')
 
-const heading = document.querySelector("#headOne")
-
-function changeHead(form){
-    const newText = form.newText.value 
-    heading.innerHTML = newText
+const changeHead = function(ev){
+    ev.preventDefault()
+    const heading = document.querySelector("#headOne")
+    const f = ev.target
+    const newText = f.newText.value
+    heading.textContent = newText
 
     
 }
 
-
-/*function changeText(){
-    //use debbuger to pause execution and work in console
-    heading.textContent = 'Different second heading'
-    /*const paragraph = document.querySelector('p.greeting')
-
-    //this works only when dealing with text
-    paragraph.textContent = 'You done clicked'
-}*/
-
+form.addEventListener('submit', changeHead)
