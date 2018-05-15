15fb2cdb4b0707b8b7d3bbdff43d8c451eac49bc
Christine Schulte <christinerschulte@gmail.com>
2018-05-15 13:02:21 -0400

    Add form that changes heading to input text when submitted

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const button = document.querySelector('button.greeting')
-const heading = document.querySelector('#headTwo')
-function changeText(){
+
+const heading = document.querySelector("#headOne")
+
+function changeHead(form){
+    const newText = form.newText.value 
+    heading.innerHTML = newText
+
+    
+}
+
+
+/*function changeText(){
     //use debbuger to pause execution and work in console
     heading.textContent = 'Different second heading'
     /*const paragraph = document.querySelector('p.greeting')
 
     //this works only when dealing with text
-    paragraph.textContent = 'You done clicked'*/
-}
+    paragraph.textContent = 'You done clicked'
+}*/
 
-button.addEventListener('click', changeText)
