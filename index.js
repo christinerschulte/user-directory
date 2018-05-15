08168c6b908e7cb9e67783f28f8ea34156bd72e2
Christine Schulte <christinerschulte@gmail.com>
2018-05-15 15:09:36 -0400

    Add age field

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const handleSubmit = function(ev){
     const users = document.querySelector("#users")
     const f = ev.target
     const userName = f.newText.value
-    users.innerHTML += '<p>' + userName + '</p>'
+    const age = f.age.value
+    users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
     f.newText.value = ''
+
 
     
 }
